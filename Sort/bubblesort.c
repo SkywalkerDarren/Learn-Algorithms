85d79dd4d00fbeb0f4f0c7813cfaa30a9d91ce65
SkywlkerDarren <yh322yh@163.com>
2017-04-05 14:34:17 +0800

    bubblesort

--- Sort/bubblesort.c
+++ Sort/bubblesort.c
@@ -11,6 +11,41 @@ void initArray(int array[]);
 void print(int array[]);
 
 int main(){
+    srand(time(NULL));
+    int array[SIZE];
+    initArray(array);
+    printf("before\n");
+    print(array);
+    bubbleSort(array);
+    printf("\nafter\n");
+    print(array);
     return 0;
 }
 
+int random(){
+    return rand() % MAX;
+}
+
+void initArray(int array[]){
+    for(int i = 0; i < SIZE; ++i){
+        array[i] = random();
+    }
+}
+
+void print(int array[]){
+    for(int i = 0; i < SIZE; ++i){
+        printf("%d ", array[i]);
+    }
+}
+
+void bubbleSort(int array[]){
+    for(int i = 0; i < SIZE; ++i){
+        for(int j = i; j < SIZE; ++j){
+            if(array[i] > array[j]){
+                int t = array[i];
+                array[i] = array[j];
+                array[j] = t;
+            }
+        }
+    }
+}
